9e7226444cb11b4a8a95e5695cadceb60ff911c0
Harsha Suryavamshi <harsha@minebrat.com>
2019-01-08 22:27:07 +0530

    Update

--- app.js
+++ app.js
@@ -78,5 +78,6 @@ server.listen(config.port, function(){
 // Defining a router
 var router = {
     'ping' : handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens
 };
